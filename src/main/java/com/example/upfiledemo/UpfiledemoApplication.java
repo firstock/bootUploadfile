1f41f499b6e6db436c28f0a5b3b398fdc655b2eb
hyungjunKim <datahj@doople.net>
2018-08-21 10:53:36 +0900

    SUCCESS_uploadFile_FAIL_testIntegration

---
+++ src/main/java/com/example/upfiledemo/UpfiledemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.upfiledemo;
+
+import com.example.upfiledemo.storage.StorageProperties;
+import com.example.upfiledemo.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UpfiledemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpfiledemoApplication.class, args);
+    }
+
+    CommandLineRunner init(StorageService storageService){
+        return (args) -> {
+          storageService.deleteAll();
+          storageService.init();
+        };
+    }
+}
