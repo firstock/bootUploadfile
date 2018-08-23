334683b7f79eb9d719b942fc83d768d53e1a16dc
hyungjunKim <datahj@doople.net>
2018-08-23 14:10:38 +0900

    add myBanner- interest

--- src/main/java/com/example/upfiledemo/storage/ConfigType.java
+++ src/main/java/com/example/upfiledemo/storage/ConfigType.java
@@ -1,6 +1,10 @@
 package com.example.upfiledemo.storage;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.xml.bind.annotation.*;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
  * @site http://www.doople.net:9999/InfiniA/ConfigManager/post/2
@@ -10,34 +14,52 @@ import javax.xml.bind.annotation.*;
 @XmlType(name= "ConfigType")
 public class ConfigType {
 
-    @XmlElement(name= "type")
-    protected String type;
-    @XmlElement(name= "process")
-    protected String process;
-    @XmlElement(name= "configKey")
-    protected String configKey;
-    @XmlAttribute(name= "port", required= true)
-    protected int port;
-    @XmlAttribute(name= "oamPort", required= true)
-    protected int oamPort;
+    @Getter @Setter
+    @XmlElement(name="Seq", required= true)
+    protected int Seq;
+    @Getter @Setter
+    @XmlElement(name="Product")
+    protected String Product;
+    @Getter @Setter
+    @XmlElement(name="ProductKey")
+    protected String ProductKey;
+    @Getter @Setter
+    @XmlElement(name="Status")
+    protected String Status;
+    @Getter @Setter
+    @XmlElement(name="IsReadOnly")
+    protected boolean IsReadOnly;
+    @Getter @Setter
+    @XmlElement(name="IsPair")
+    protected boolean IsPair;
+    @Getter @Setter
+    @XmlElement(name="IsNull")
+    protected boolean IsNull;
+    @Getter @Setter
+    @XmlElement(name="IsRestart")
+    protected boolean IsRestart;
+    @Getter @Setter
+    @XmlElement(name="IsNecessary")
+    protected boolean IsNecessary;
+    @Getter @Setter
+    @XmlElement(name="Type")
+    protected String Type;
+    @Getter @Setter
+    @XmlElement(name="ConfigKey")
+    protected String ConfigKey;
+    @Getter @Setter
+    @XmlElement(name="ConfigValue")
+    protected String ConfigValue;
+    @Getter @Setter
+    @XmlElement(name="Description")
+    protected String Description;
+    @Getter @Setter
+    @XmlElement(name="ConfigManagerIP")
+    protected String ConfigManagerIP;
+    @Getter @Setter
+    @XmlElement(name="CreateDate")
+    protected XMLGregorianCalendar CreateDate;
+    @Getter @Setter
+    @XmlElement(name="UpdateDate")
+    protected XMLGregorianCalendar UpdateDate;
 }
-
-/*
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "StateType", propOrder = {
-    "description",
-    "state",
-    "stateMachine"
-})
-public class StateType {
-
-    @XmlElement(name = "Description", required = true)
-    protected String description;
-    @XmlElement(name = "State")
-    protected List<StateType> state;
-    @XmlElement(name = "StateMachine")
-    protected List<StateMachineType> stateMachine;
-    @XmlAttribute(name = "stateId", required = true)
-    protected String stateId;
-    @XmlAttribute(name = "stateName", required = true)
-    protected String stateName;*/
