ae4634c137e4fbdc0975c47a5198d70fc75efb47
hyungjunKim <datahj@doople.net>
2018-08-24 19:07:07 +0900

    mod code to comment - nothing change

--- src/main/java/com/example/upfiledemo/storage/ConfigType.java
+++ src/main/java/com/example/upfiledemo/storage/ConfigType.java
@@ -20,46 +20,46 @@ public class ConfigType {
     @Getter @Setter
     @XmlElement(name="Product")
     protected String Product;
-    @Getter @Setter
-    @XmlElement(name="ProductKey")
-    protected String ProductKey;
-    @Getter @Setter
-    @XmlElement(name="Status")
-    protected String Status;
-    @Getter @Setter
-    @XmlElement(name="IsReadOnly")
-    protected boolean IsReadOnly;
-    @Getter @Setter
-    @XmlElement(name="IsPair")
-    protected boolean IsPair;
-    @Getter @Setter
-    @XmlElement(name="IsNull")
-    protected boolean IsNull;
-    @Getter @Setter
-    @XmlElement(name="IsRestart")
-    protected boolean IsRestart;
-    @Getter @Setter
-    @XmlElement(name="IsNecessary")
-    protected boolean IsNecessary;
-    @Getter @Setter
-    @XmlElement(name="Type")
-    protected String Type;
-    @Getter @Setter
-    @XmlElement(name="ConfigKey")
-    protected String ConfigKey;
-    @Getter @Setter
-    @XmlElement(name="ConfigValue")
-    protected String ConfigValue;
-    @Getter @Setter
-    @XmlElement(name="Description")
-    protected String Description;
-    @Getter @Setter
-    @XmlElement(name="ConfigManagerIP")
-    protected String ConfigManagerIP;
-    @Getter @Setter
-    @XmlElement(name="CreateDate")
-    protected XMLGregorianCalendar CreateDate;
-    @Getter @Setter
-    @XmlElement(name="UpdateDate")
-    protected XMLGregorianCalendar UpdateDate;
+//    @Getter @Setter
+//    @XmlElement(name="ProductKey")
+//    protected String ProductKey;
+//    @Getter @Setter
+//    @XmlElement(name="Status")
+//    protected String Status;
+//    @Getter @Setter
+//    @XmlElement(name="IsReadOnly")
+//    protected boolean IsReadOnly;
+//    @Getter @Setter
+//    @XmlElement(name="IsPair")
+//    protected boolean IsPair;
+//    @Getter @Setter
+//    @XmlElement(name="IsNull")
+//    protected boolean IsNull;
+//    @Getter @Setter
+//    @XmlElement(name="IsRestart")
+//    protected boolean IsRestart;
+//    @Getter @Setter
+//    @XmlElement(name="IsNecessary")
+//    protected boolean IsNecessary;
+//    @Getter @Setter
+//    @XmlElement(name="Type")
+//    protected String Type;
+//    @Getter @Setter
+//    @XmlElement(name="ConfigKey")
+//    protected String ConfigKey;
+//    @Getter @Setter
+//    @XmlElement(name="ConfigValue")
+//    protected String ConfigValue;
+//    @Getter @Setter
+//    @XmlElement(name="Description")
+//    protected String Description;
+//    @Getter @Setter
+//    @XmlElement(name="ConfigManagerIP")
+//    protected String ConfigManagerIP;
+//    @Getter @Setter
+//    @XmlElement(name="CreateDate")
+//    protected XMLGregorianCalendar CreateDate;
+//    @Getter @Setter
+//    @XmlElement(name="UpdateDate")
+//    protected XMLGregorianCalendar UpdateDate;
 }
