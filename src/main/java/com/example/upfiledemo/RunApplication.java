bd1ec44d8a1a4b409b8a23b5f0db4439dafa4c51
hyungjunKim <datahj@doople.net>
2018-09-06 13:49:16 +0900

    addCode_xml son2_수동으로 하나 더

--- src/main/java/com/example/upfiledemo/RunApplication.java
+++ src/main/java/com/example/upfiledemo/RunApplication.java
@@ -20,20 +20,28 @@ public class RunApplication {
         Processor processor = ctx.getBean(Processor.class);
 
         //Perform Marshaling
-        Company company1 = new Company(){};
-        Code code1= new Code();
+        Company company = new Company(){}; // parent
+        Code code1= new Code(); // child1
+        final String CODE_ID= "CM_MSG_CODE_000";
+        int codeIdNum= 1;
 
-        code1.setId("CM_MSG_CODE_0001");
+        code1.setId(CODE_ID + codeIdNum++);
         code1.setKor("HAS Url을 입력하세요.");
         code1.setEng("Please enter HAS Url.");
-        company1.getCode().add(code1);
+        company.getCode().add(code1);
 
+        Code code2= new Code(); // child1
 
-        processor.objectToXML("country.xml", company1);
+        code2.setId(CODE_ID + codeIdNum++);
+        code2.setKor("입력한 Url 주소가 정상적이지 않습니다.다시 입력하세요.");
+        code2.setEng("The Url address you entered is not correct. Please enter it again.");
+        company.getCode().add(code2);
+
+        processor.objectToXML("country.xml", company);
 
         LOG.info("Marshaling performed");
         //Perform UnMarshaling
-        company1 = (Company)processor.xmlToObject("country.xml");
-        LOG.info("After UnMarshaling Data is: untryName:"+company1.getCode());
+        company = (Company)processor.xmlToObject("country.xml");
+        LOG.info("After UnMarshaling Data is: untryName:"+company.getCode());
     }
 }
